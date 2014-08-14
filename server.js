3b5a0a1bf25ba79b2cb693fbadcecd9697d7aea2
Gianpaulo <jam_pow@hotmail.com>
2014-08-14 17:26:01 -0300

    changing the urls

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-app.get( '/primeira-url-fake.html', function( req, res ){ res.redirect( '/index.html' ); });
-app.get( '/segunda-url-fake.html' , function( req, res ){ res.redirect( '/index.html' ); });
-app.get( '/terceira-url-fake.html', function( req, res ){ res.redirect( '/index.html' ); });
+app.get( '/first-url.html', function( req, res ){ res.redirect( '/index.html' ); });
+app.get( '/second-url.html' , function( req, res ){ res.redirect( '/index.html' ); });
+app.get( '/third-url.html', function( req, res ){ res.redirect( '/index.html' ); });
 
 app.listen(process.env.PORT || 3000);
